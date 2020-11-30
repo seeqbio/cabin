10440224ac72d2fba61f5eff0c3c5fcd81f34e2b
Maia <maia@streamlinegenomics.com>
2020-11-30 20:00:40 +0000

    Add basic dummy dataset & import, biodb2, refs DEV-788, DEV-788

---
+++ cabin/data/datasets/test_dataset.py
@@ -0,0 +1,68 @@
+from biodb.mixins import RecordByRecordImportMixin
+from biodb.data.db import ImportedTable
+from biodb.data.files import LocalFile, ExternalFile
+from biodb.io import read_xsv, wget
+from biodb import settings
+from pathlib import Path
+
+class TestDatasetOfficial(ExternalFile):
+    version = 'human.head'
+
+    @property
+    def url(self):
+        return 'https://gist.githubusercontent.com/amirkdv/' \
+               '85e91edf4b032ec5460177c44c36b446/raw/0ae6efdf95ecfb62b7a534bafa324120a7119779/gene2refseq_{v}'.format(v=self.version)
+
+
+class TestDatasetFile(LocalFile):
+    version = '1'
+    depends = {'TestDatasetOfficial': TestDatasetOfficial}
+    extension = 'txt'
+
+
+#    @property
+#    def download_path(self):
+#        return Path(settings.SGX_DOWNLOAD_DIR) / self.name  # Dataset from core has this
+
+    def produce(self):
+        wget(self.input.url, self.path)  # path for LocalFile is in mock_storage
+
+
+class TestDatasetTable(ImportedTable, RecordByRecordImportMixin):
+    version = '10'
+    depends = {'TestDatasetFile': TestDatasetFile}
+
+    columns = [
+        'GeneID',
+        'Symbol',
+    ]
+
+
+    @property
+    def schema_path(self):  # TODO: absorbe schema into this directly
+        return settings.SGX_SCHEMA_DIR / 'gene2refseq.sql'
+
+    @property
+    def sql_create(self):
+        pass  # needed for base's method that is needed by import_real  
+        # with self.schema_path.open() as f:
+            # return f.read().format(table=self.table_name)
+ 
+    @property
+    def schema(self):
+        return """
+            CREATE TABLE `{table}` (
+                GeneID                                         VARCHAR(255) NOT NULL,    -- NCBI gene identifier (integer stored as string)
+                Symbol                                         VARCHAR(255) NOT NULL,    -- HGNC gene symbol
+                INDEX (Symbol)
+            );
+        """
+
+
+    def produce(self):
+        self.create_table()
+        self.import_real()
+
+    def read(self):
+        for row in read_xsv(self.inputs['TestDatasetFile'].path):
+            yield row
