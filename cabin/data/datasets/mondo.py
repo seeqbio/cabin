4de0d4b78f726111887dd986118c67ac3928a51d
Maia <maia@streamlinegenomics.com>
2021-01-06 18:55:01 +0000

    Port dataset: mondo, refs DEV-864

---
+++ cabin/data/datasets/mondo.py
@@ -0,0 +1,54 @@
+from biodb.data.db import RecordByRecordImportedTable
+from biodb.data.files import LocalFile, ExternalFile
+from biodb.io import read_obo
+
+
+class mondoOfficial(ExternalFile):
+    version = 'v2020-09-14'
+
+    @property
+    def url(self):
+        # release: https://github.com/monarch-initiative/mondo/releases
+        return 'https://github.com/monarch-initiative/mondo/releases/download/{v}/mondo.obo'.format(v=self.version)
+
+
+class mondoFile(LocalFile):
+    version = '1'
+    depends = [mondoOfficial]
+    extension = 'obo'
+
+
+class mondoTable(RecordByRecordImportedTable):
+    version = '1'
+    depends = [mondoFile]
+
+    columns = ['name', 'id', 'children', 'parents', 'hpo_ids', 'do_ids', 'omim_ids', 'mesh_ids']
+
+    @property
+    def schema(self):
+        return """
+          CREATE TABLE `{table}` (
+            name       VARCHAR(255)   NOT NULL,         -- human readable disease name
+            id         VARCHAR(255)   PRIMARY KEY,      -- unique identifier, eg: MONDO:0004355
+            parents    VARCHAR(255)   NOT NULL,         -- pipe separated immediate parent
+            children   VARCHAR(32768) NOT NULL,         -- pipe separated immediate children
+            hpo_ids    VARCHAR(255)   NOT NULL,         -- xrefs to Human Phenotype Ontology
+            do_ids     VARCHAR(255)   NOT NULL,         -- xrefs to Disease Ontology
+            omim_ids   VARCHAR(255)   NOT NULL,         -- xrefs to Online Mendelian Inheritance in Man
+            mesh_ids   VARCHAR(255)   NOT NULL          -- xrefs to Medical Subject Headings
+        );
+        """
+
+    def read(self):
+        for term in read_obo(str(self.input.path)):
+            yield {
+                'id': term['id'],
+                'name': term['name'],
+                'children': '|'.join(term['children']),
+                'parents': '|'.join(term['parents']),
+                'do_ids': '|'.join(xref for xref in term['xrefs'] if xref.startswith('DOID:')),
+                'hpo_ids': '|'.join(xref for xref in term['xrefs'] if xref.startswith('HP:')),
+                'omim_ids': '|'.join(xref for xref in term['xrefs'] if xref.startswith('OMIM:')),
+                'mesh_ids': '|'.join(xref for xref in term['xrefs'] if xref.startswith('MESH:'))
+
+            }
