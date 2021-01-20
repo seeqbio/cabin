17c8c837c91d9b13cc6fda2ac839ac6d533d43e8
Amir Kadivar <amir@amirkdv.ca>
2021-01-20 15:01:13 -0500

    Retire all biodb 0.1 code, bin/biodb is not biodb 0.2, refs DEV-880

--- cabin/app.py
+++ cabin/app.py
@@ -1,7 +1,7 @@
 import sys
-import fnmatch
 import logging
 import argparse
+import fnmatch
 from abc import ABC
 from abc import abstractmethod
 from collections import OrderedDict
@@ -21,47 +21,24 @@ from biodb import AbstractAttribute
 from biodb.mysql import MYSQL
 from biodb.mysql import READER
 
-from biodb.base import DatasetVersion
-from biodb.datasets.test_dataset import test_dataset
-from biodb.datasets.gene_info import gene_info
-from biodb.datasets.refseq_summary import refseq_summary
-from biodb.datasets.gene2refseq import gene2refseq
-from biodb.datasets.gene2ensembl import gene2ensembl
-from biodb.datasets.dbSNP import dbSNP
-from biodb.datasets.dbNSFP import dbNSFP
-from biodb.datasets.ClinVar import ClinVarVCF
-from biodb.datasets.ClinVar import ClinVarHGVS4Variation
-from biodb.datasets.ClinVar import ClinVarSCV
-from biodb.datasets.ClinVar import ClinVarRCV
-from biodb.datasets.ensembl import ENSEMBL_DATASETS
-from biodb.datasets.CancerMine import CancerMineCollated
-from biodb.datasets.CancerMine import CancerMineSentences
-from biodb.datasets.CIViC import CIViC
-from biodb.datasets.hpo import hpo
-from biodb.datasets.disease_ontology import DiseaseOntology
-from biodb.datasets.mondo import mondo
-from biodb.datasets.pmkb import pmkb
-from biodb.datasets.pfam import Pfam
-from biodb.datasets.pharmGKB import pharmGKB
-from biodb.datasets.dgi import dgi
-from biodb.datasets.medgen import medgenNames
-from biodb.datasets.medgen import medgenDefn
-from biodb.datasets.medgen import medgenHPO
+from biodb.data import registry
+from biodb.data.db import ImportedTable
+from biodb.data.registry import load_table_registry
 
 
-class UsageError(Exception):
-    pass
+def glob_matching_classes(dataset):
+    matching_classes = []
+    for ds_class in registry.TYPE_REGISTRY:
+        if (issubclass(registry.TYPE_REGISTRY[ds_class], ImportedTable)):
+            if fnmatch.fnmatch(ds_class, dataset):
+                matching_classes.append(ds_class)
+    return matching_classes
 
 
 class AppCommand(ABC):
     name = AbstractAttribute()
     help = None
     description = None
-
-    dataset_help = 'The internal name for a dataset, e.g. "ClinVar"'
-    source_version_help = 'The version of a dataset as represented by data source'
-    checksum_help = 'The sha256 checksum of downloaded file contents'
-    git_version_help = 'The version of biodb used for importing a dataset'
 
     def __init__(self, app=None):
         description = self.description if self.description else self.help
@@ -75,10 +52,18 @@ class AppCommand(ABC):
 
 class InitCommand(AppCommand):
     name = "init"
-    help = "initialize databases and users"
+    help = "initialize database, users, and build system table"
 
     def run(self):
         MYSQL.initialize()
+
+
+class ListCommand(AppCommand):
+    name = "list"
+    help = "list all datasets for which a handler exists"
+
+    def run(self):
+        logger.info('\n'.join(c for c in registry.TYPE_REGISTRY))
 
 
 class DropUsersCommand(AppCommand):
@@ -87,6 +72,44 @@ class DropUsersCommand(AppCommand):
 
     def run(self):
         MYSQL.drop_users()
+
+
+class DropCommand(AppCommand):
+    name = "drop"
+    help = "inverse of 'import', drops table from db and `system` table."
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.parser.add_argument('dataset')
+
+    def run(self):
+        for _, hdataset in load_table_registry().items():
+            if fnmatch.fnmatch(hdataset.name, self.app.args.dataset):
+                logger.info("Dropping table: %s" % hdataset.name)
+                hdataset.drop()
+
+
+class ImportCommand(AppCommand):
+    name = "import"
+    help = "import a dataset into database"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.parser.add_argument('dataset')
+        self.parser.add_argument('-n', '--dry-run', action='store_true', help="do not actually import, just show a synopsis")
+
+    def run(self):
+
+        classes = glob_matching_classes(self.app.args.dataset)
+
+        if not classes:
+            logger.error("No Tables in registry matching %s." % self.app.args.dataset)
+            return 1
+        else:
+            logger.info("Tables to import: %s" % classes)
+        for ds_name in classes:
+            ds = getattr(registry, ds_name)()
+            ds.produce_recursive(dry_run=self.app.args.dry_run)
 
 
 class ShellCommand(AppCommand):
@@ -98,72 +121,7 @@ class ShellCommand(AppCommand):
         self.parser.add_argument('--user', '-u', default=READER, help="user to log in to MySQL")
 
     def run(self):
-        """Executes mysql client in _this_ process (replaces python process
-        immediately). This is necessary; using a subprocess produces weird
-        behavior with how interrupts are handled, e.g. how ctrl+c works.
-
-        Note on `os.execvp` usage:
-            The first argument to execvp is the executable name (searched for
-            in $PATH) and the second argument is the argv list passed to the
-            process.  The latter includes another copy of the executable name
-            since that, too, is passed as argv to the executable.
-        """
         MYSQL.shell(self.app.args.user) # execvp to mysql client
-
-
-class ListCommand(AppCommand):
-    name = "list"
-    help = "list all datasets for which a handler exists"
-
-    def run(self):
-        classes = [cls.name for cls in self.app.dataset_classes]
-        print('\n'.join(classes))
-
-
-class ImportCommand(AppCommand):
-    name = "import"
-    help = "import a dataset into database"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.parser.add_argument('dataset', help=self.dataset_help + '; accepts glob, e.g. "ensembl.*"')
-        self.parser.add_argument('source_version', help=self.source_version_help)
-        self.parser.add_argument('checksum', nargs='?', help=self.checksum_help)
-
-    def run(self):
-        # fnmatch is the internal mechanism used by glob; currently only
-        # works for "ensembl.*" (and not, say, "gene2ref*")
-        classes = [ds_class for ds_class in self.app.dataset_classes
-                   if fnmatch.fnmatch(ds_class.name, self.app.args.dataset)]
-        if not classes:
-            logger.error('unknown dataset pattern "%s"' % self.app.args.dataset)
-            return 1
-        for ds_class in classes:
-            ds_name = ds_class.name
-            ds_version = DatasetVersion(source=self.app.args.source_version,
-                                        checksum=self.app.args.checksum)
-            ds = self.app.dataset(name=ds_name, version=ds_version)
-            ds.import_()
-            ds.archive()
-
-
-class DropCommand(AppCommand):
-    name = "drop"
-    help = "drop a dataset from database"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.parser.add_argument('dataset', help=self.dataset_help)
-        self.parser.add_argument('source_version', help=self.source_version_help)
-        self.parser.add_argument('checksum', help=self.checksum_help)
-        self.parser.add_argument('git_version', help=self.git_version_help)
-
-    def run(self):
-        ds_version = DatasetVersion(source=self.app.args.source_version,
-                                    checksum=self.app.args.checksum,
-                                    git=self.app.args.git_version)
-        ds = self.app.dataset(name=self.app.args.dataset, version=ds_version)
-        ds.drop()
 
 
 class StatusCommand(AppCommand):
@@ -172,116 +130,46 @@ class StatusCommand(AppCommand):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.parser.add_argument('dataset', nargs='?', help=self.dataset_help)
+        self.parser.add_argument('dataset', nargs='?')
 
     def run(self):
         def yesno(val):
             return 'yes' if val else 'no'
 
         width_by_column = OrderedDict([
-            ('name',        27),
-            ('version',     15),
-            ('checksum',    12),
-            ('code',        30),
-            ('download',    10),
-            ('archive',     10),
-            ('import',      10),
+            ('type',         32),
+            ('version',      9),
+            ('depends',      38),
+            ('latest',       10),
+            ('Full name',    50),
         ])
         columns = width_by_column.keys()
         fmt_string = ''.join('{%s:%d}' % (col, width) for col, width in width_by_column.items())
 
         # header line
-        print(fmt_string.format(**dict(zip(columns, columns))))
+        logger.info(fmt_string.format(**dict(zip(columns, columns))))
 
         # content lines
-        token = self.app.args.dataset
-        for cls in self.app.dataset_classes:
-            if token is not None and cls.name[:len(token)] != token:
+        if self.app.args.dataset is not None:
+            classes = glob_matching_classes(self.app.args.dataset)
+        else:
+            classes = []
+
+        for _, hdataset in sorted(load_table_registry().items()):
+            if self.app.args.dataset is not None and hdataset.type not in classes:
                 continue
-
-            for dataset in cls.search_sorted():
-                row = [
-                    dataset.name,
-                    dataset.version.source,
-                    dataset.version.checksum,
-                    dataset.version.git,
-                    yesno(dataset.downloaded),
-                    yesno(dataset.archived),
-                    yesno(dataset.imported)
-                ]
-                row = [str(x) if x else '' for x in row]
-                print(fmt_string.format(**dict(zip(columns, row))))
-
-
-class DownloadCommand(AppCommand):
-    name = "download"
-    help = "download a dataset from source or archives"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.parser.add_argument('dataset', help=self.dataset_help)
-        self.parser.add_argument('source_version', help=self.source_version_help)
-        self.parser.add_argument('checksum', nargs='?', help=self.checksum_help)
-
-    def run(self):
-        version = DatasetVersion(source=self.app.args.source_version,
-                                 checksum=self.app.args.checksum)
-        ds = self.app.dataset(name=self.app.args.dataset, version=version)
-        ds.download()
-
-
-class ArchiveCommand(AppCommand):
-    name = "archive"
-    help = "update archives for a dataset"
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.parser.add_argument('dataset', help=self.dataset_help)
-        self.parser.add_argument('source_version', help=self.source_version_help)
-        self.parser.add_argument('checksum', help=self.checksum_help)
-
-    def run(self):
-        version = DatasetVersion(source=self.app.args.source_version,
-                                 checksum=self.app.args.checksum)
-        ds = self.app.dataset(name=self.app.args.dataset, version=version)
-        ds.archive()
+            row = [
+                hdataset.type,
+                hdataset.formula['version'], # TODO: consider adding to historical dataset as atribute
+                list(hdataset.formula['inputs'].keys()), # TODO: consider', '.join(c.__name__ for c in hdataset.depends),
+                hdataset.is_latest(),
+                hdataset.name,
+            ]
+            row = [str(x) if x else '' for x in row]
+            logger.info(fmt_string.format(**dict(zip(columns, row))))
 
 
 class App:
-    dataset_classes = [
-        CancerMineCollated,
-        CancerMineSentences,
-        test_dataset,
-        gene_info,
-        refseq_summary,
-        gene2refseq,
-        gene2ensembl,
-        ClinVarVCF,
-        ClinVarHGVS4Variation,
-        ClinVarSCV,
-        ClinVarRCV,
-        CIViC,
-        dbNSFP,
-        dbSNP,
-        hpo,
-        DiseaseOntology,
-        mondo,
-        pmkb,
-        Pfam,
-        pharmGKB,
-        dgi,
-        medgenNames,
-        medgenDefn,
-        medgenHPO
-    ] + list(ENSEMBL_DATASETS.values())
-
-    def dataset(self, name, version):
-        for cls in self.dataset_classes:
-            if cls.name == name:
-                return cls(version=version)
-        else:
-            raise BiodbError('Unknown dataset ' + name)
-
     def __init__(self):
         parser = argparse.ArgumentParser(description="""
             Versioned importer of datasets into MySQL with S3 archiving.
@@ -293,22 +181,23 @@ class App:
         self.cmd_parser = parser.add_subparsers(title='commands', dest='command')
         self.parser = parser
 
+        # TODO consider refactor: stop passing app into commands, pass args to
+        # their run()
         self.commands = {
-            'shell':    ShellCommand(app=self),
-            'init':     InitCommand(app=self),
-            'list':     ListCommand(app=self),
-            'import':   ImportCommand(app=self),
-            'drop':     DropCommand(app=self),
-            'status':   StatusCommand(app=self),
-            'download': DownloadCommand(app=self),
-            'archive':  ArchiveCommand(app=self),
-            'drop-users':   DropUsersCommand(app=self),
+            'shell':            ShellCommand(app=self),
+            'init':             InitCommand(app=self),
+            'list':             ListCommand(app=self),
+            'drop-users':       DropUsersCommand(app=self),
+            'import':           ImportCommand(app=self),
+            'drop':             DropCommand(app=self),
+            'status':           StatusCommand(app=self)
+
         }
 
     def init(self, *argv):
         self.args = self.parser.parse_args(argv)
         if self.args.debug:
-            logger.setLevel(logging.debug)
+            logger.setLevel(logging.DEBUG)
 
     def run(self, *argv):
         self.init(*argv)
--- cabin/settings.py
+++ cabin/settings.py
@@ -8,12 +8,6 @@ SGX_MYSQL_DB = 'sgx_biodb'
 SGX_MYSQL_READER_PASSWORD = 'streamline'
 SGX_MYSQL_WRITER_PASSWORD = 'streamline'
 
-SGX_S3_ARCHIVE_BUCKET = os.environ.get('SGX_S3_ARCHIVE_BUCKET', 'sgx-archives')
-SGX_S3_ARCHIVE_PREFIX = os.environ.get('SGX_S3_ARCHIVE_PREFIX', 'database/')
-
-SGX_S3_CACHE_BUCKET = os.environ.get('SGX_S3_CACHE_BUCKET', 'sgx-cache')
-SGX_S3_CACHE_PREFIX = os.environ.get('SGX_S3_CACHE_PREFIX', 'biodb/')
-
 SGX_DOWNLOAD_DIR = os.environ.get('SGX_DOWNLOAD_DIR', 'downloads')
 
 SGX_MYSQL_PROFILE = False
