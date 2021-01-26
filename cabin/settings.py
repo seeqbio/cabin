06fa2f3b71b076370d6d80f73a5444d59b2f6a6c
Amir Kadivar <amir@amirkdv.ca>
2021-01-26 15:13:07 -0500

    Clean up default settings for download dir and mysql host

--- cabin/settings.py
+++ cabin/settings.py
@@ -3,13 +3,12 @@ from pathlib import Path
 
 # TODO document
 
-SGX_MYSQL_HOST = os.environ.get('SGX_MYSQL_HOST', 'localhost')
+SGX_MYSQL_HOST = 'mysql'
 SGX_MYSQL_DB = 'sgx_biodb'
 SGX_MYSQL_READER_PASSWORD = 'streamline'
 SGX_MYSQL_WRITER_PASSWORD = 'streamline'
 
-SGX_DOWNLOAD_DIR = os.environ.get('SGX_DOWNLOAD_DIR', 'downloads')
-
+SGX_DOWNLOAD_DIR = 'downloads'
 SGX_MYSQL_PROFILE = False
 
 SGX_ROOT_DIR = Path(__file__).parents[1].absolute()
