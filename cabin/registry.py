b68609622db023388e926d115a73237b64ea7b68
Amir Kadivar <amir@amirkdv.ca>
2021-07-23 00:29:47 -0400

    Refactor implementation of biodb.registry, refs #19
    The existing implementation performed the equivalent of relative imports
    in its magic which would then clobber imports from the Seeq app.
    
    Steps to reproduce: in FastAPI code, i.e. in `seeq/`, add the following
    line anywhere after biodb.registry is imported:
    
        import seeq
    
    This will give you the package biodb.datasets.seeq instead of the
    package seeq, in repo top level, as expected.

--- cabin/registry.py
+++ cabin/registry.py
@@ -1,39 +1,59 @@
 """
-Implements import magic:
+Implements import magic. Usage:
 
-    * Any concrete Dataset subclass defined anywhere in biodb.db.datasets is
-      imported and added to the symbol table of this module so that you can do
-      things like: `from biodb.registry import MyDatasetClass` regardless of
-      where in the datasets package MyDatasetClass is defined.
-    * Provides TYPE_REGISTRY which is a {cls.name => cls} dictionary of all
-      defined Dataset classes.
-    * Provides TABLE_REGISTRY which is a {dataset.name => dataset} dictionary of
-      all datasets that are imported in the database.
+    from biodb.registry import CIViCTable
+    # just works, regardless of where in biodb.datasets the class is defined
+
+    from biodb.registry import TYPE_REGISTRY
+    TYPE_REGISTRY['CIViCTable'] == CIViCTable   # True
 """
+from typing import Dict, Type
 import pkgutil
 import inspect
+import importlib
 
-from . import datasets
 from .core import Dataset
-from .db import imported_datasets
 
 
 def get_class_path(cls):
-    # given a class object, returns the absolute path to the module that defines it
+    """Given a class object, returns the absolute path to the module that defines it."""
     return inspect.getmodule(cls).__file__
 
 
-def load_dataset_classes(pkg):
-    # recursively finds all datasets defined in the biodb.datasets package
+def import_dataset_classes(root_name: str='biodb.datasets') -> Dict[str, Type]:
+    """Recursively finds all Dataset classes defined in the root package and
+    imports them. Returns a dictionary of Dataset type, i.e. class name, to
+    Class object.
+    """
+
+    # important implementation detail: we want to always refer to modules we
+    # import here in full name all the way to root_name. This means, for
+    # example, performing the equivalent of
+    #
+    #       import biodb.datatasets.CIViC
+    #
+    # in order to get at the classes in that module, and *not* the equivalent of
+    #
+    #       from biodb.datasets import CIViC
+    #
+    # This way we can easily clobber our caller's unrelated imports if they
+    # have modules with similar names. After the equivalent of the above
+    # relative import gets executed, anyone asking for
+    #
+    #       import CIViC
+    #
+    # will get the module we import here.
+    pkg = importlib.import_module(root_name)
     classes = {}
-    for importer, modname, ispkg in pkgutil.walk_packages(pkg.__path__):
-        mod = importer.find_module(modname).load_module(modname)
+    for importer, modname, ispkg in pkgutil.walk_packages(path=pkg.__path__, prefix=root_name + '.'):
+        mod = importlib.import_module(modname) # e.g. biodb.datasets.CIViC
         for name, cls in inspect.getmembers(mod):
             if not inspect.isclass(cls):
                 continue
             # exclude any Dataset without a proper version (e.g. None).
             # This is a proxy for (abstract) base classes.
             if issubclass(cls, Dataset) and getattr(cls, 'version', None):
+                # e.g. CIViCTable from biodb.datasets.CIViC
                 name = cls.__name__
                 if name in classes:
                     # enforce class name uniqueness but don't choke on the same
@@ -50,7 +70,7 @@ def load_dataset_classes(pkg):
     return classes
 
 
-TYPE_REGISTRY = load_dataset_classes(datasets)
+TYPE_REGISTRY = import_dataset_classes()
 for name, cls in TYPE_REGISTRY.items():
     globals()[name] = cls
 
