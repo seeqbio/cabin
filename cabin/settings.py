7cccad944fb69df2a4c2677749739d01083275ed
maia <maia@streamlinegenomics.com>
2022-01-11 13:33:24 -0500

    Update settings

--- cabin/settings.py
+++ cabin/settings.py
@@ -1,19 +1,19 @@
 import os
 
-# see ops.SGX_INSTANCE_ID
-SGX_INSTANCE_ID = '1'#os.environ.get('SGX_INSTANCE_ID')
+# see ops.CABIN_INSTANCE_ID
+CABIN_INSTANCE_ID = '1'#os.environ.get('CABIN_INSTANCE_ID')
 
-SGX_MYSQL_HOST = '127.0.0.1' # 'mysql'
-SGX_MYSQL_DB = 'cabin'
-SGX_MYSQL_READER_USER = 'cabin'
-SGX_MYSQL_WRITER_USER = 'cabin'
-SGX_MYSQL_READER_PASSWORD = 'cabin'
-SGX_MYSQL_WRITER_PASSWORD = 'cabin' # os.environ.get('SGX_MYSQL_WRITER_PASSWORD')
-SGX_MYSQL_CNX_TIMEOUT = int(os.environ.get('SGX_MYSQL_CNX_TIMEOUT', 30))
+CABIN_MYSQL_HOST = os.environ.get('CABIN_DB_HOST', '127.0.0.1')
+CABIN_MYSQL_DB = os.environ.get('CABIN_DB_NAME', 'cabin')
+CABIN_MYSQL_READER_USER = 'cabin'
+CABIN_MYSQL_WRITER_USER = 'cabin'
+CABIN_MYSQL_READER_PASSWORD = 'cabin'
+CABIN_MYSQL_WRITER_PASSWORD = 'cabin'
+CABIN_MYSQL_CNX_TIMEOUT = int(os.environ.get('CABIN_MYSQL_CNX_TIMEOUT', 30))
 
-SGX_DOWNLOAD_DIR = '/sgx/cabin/downloads'
+CABIN_DOWNLOAD_DIR = os.environ.get('CABIN_DOWNLOADS', '/tmp/cabin/downloads')
 
-SGX_S3_MIRROR_BUCKET = os.environ.get('SGX_S3_MIRROR_BUCKET', 'sgx-archives')
-SGX_S3_MIRROR_PREFIX = os.environ.get('SGX_S3_MIRROR_PREFIX', 'cabin/mirrors').rstrip('/')
+CABIN_S3_MIRROR_BUCKET = os.environ.get('CABIN_S3_MIRROR_BUCKET', 'cabin-archives')
+CABIN_S3_MIRROR_PREFIX = os.environ.get('CABIN_S3_MIRROR_PREFIX', 'cabin/mirrors').rstrip('/')
 
-SGX_NON_INTERACTIVE = 'CI_PIPELINE_ID' in os.environ
+CABIN_NON_INTERACTIVE = 'CI_PIPELINE_ID' in os.environ
