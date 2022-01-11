c2cd7958dcf672f909a1b6a70464dc16432e2a36
Vincent Lortie <vincent@streamlinegenomics.com>
2022-01-11 13:55:18 -0500

    Reproducing Cabin: working state
    * Mysql was setup prior to this commit

--- cabin/settings.py
+++ cabin/settings.py
@@ -3,12 +3,12 @@ import os
 # see ops.SGX_INSTANCE_ID
 SGX_INSTANCE_ID = '1'#os.environ.get('SGX_INSTANCE_ID')
 
-SGX_MYSQL_HOST = 'localhost' # 'mysql'
+SGX_MYSQL_HOST = '127.0.0.1' # 'mysql'
 SGX_MYSQL_DB = 'cabin'
-SGX_MYSQL_READER_USER = 'reader'
-SGX_MYSQL_WRITER_USER = 'writer'
-SGX_MYSQL_READER_PASSWORD = 'KAZ'
-SGX_MYSQL_WRITER_PASSWORD = 'KAZ' # os.environ.get('SGX_MYSQL_WRITER_PASSWORD')
+SGX_MYSQL_READER_USER = 'cabin'
+SGX_MYSQL_WRITER_USER = 'cabin'
+SGX_MYSQL_READER_PASSWORD = 'password'
+SGX_MYSQL_WRITER_PASSWORD = 'password' # os.environ.get('SGX_MYSQL_WRITER_PASSWORD')
 SGX_MYSQL_CNX_TIMEOUT = int(os.environ.get('SGX_MYSQL_CNX_TIMEOUT', 30))
 
 SGX_DOWNLOAD_DIR = '/sgx/cabin/downloads'
