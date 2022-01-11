f18e2248d44f5e9a16f537c52129cd528ba52ae2
Amir Kadivar <amir@amirkdv.ca>
2022-01-11 15:28:14 -0500

    mysql/init.sh indentation

--- mysql/init.sh
+++ mysql/init.sh
@@ -5,8 +5,12 @@ SLEEP=20
 
 HERE=$(dirname $(readlink -f $0))
 
-docker run -v $HERE/init.sql:/init.sql --rm --name mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=cabin mysql/mysql-server:8.0
+docker run \
+  -v $HERE/init.sql:/init.sql \
+  -d --rm --name mysql \
+  -p 3306:3306 \
+  -e MYSQL_ROOT_PASSWORD=cabin \
+  mysql/mysql-server:8.0
 
 for i in $(seq $SLEEP); do echo -n "."; sleep 1; done
-
 docker exec mysql bash -c "cat /init.sql | mysql -u root -pcabin"
